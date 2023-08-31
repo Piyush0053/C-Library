16c32b40d86c7f928d99f208d1a1f5feb84ac1c3
Piyush0053 <mittalpiyush0053@gmail.com>
2023-08-31 10:42:27 +0530

    somequestions

---
+++ functions/someproblems.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value";
+    cin>>n;
+    int i=1;
+    while(n>0){
+        int rem=n%10;
+        cout<<rem;
+        cout<<",";
+        n=n/10;
+    }
+    while (n=0)
+    {
+    cout<<"the digits are resp."<<endl;
+    }
+    
+}
