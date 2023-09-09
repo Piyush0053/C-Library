46513f8def0423488f87e8671159c4947414ba23
Piyush0053 <mittalpiyush0053@gmail.com>
2023-09-09 23:00:48 +0530

    arrays

---
+++ arrays/L1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[53];
+char arr1[106];
+bool arr2[23];
+int arr3[10]={1,2,3,4,5,6,7,12};
+char arr4[10]={'a','b','c'};
+bool arr5[23]={0,1};
+cout<<arr<<endl;
+cout<<arr1<<endl;
+cout<<arr2<<endl;
+cout<<arr3[2]<<endl;
+cout<<arr4<<endl;
+cout<<arr5[1]<<endl;
+}
